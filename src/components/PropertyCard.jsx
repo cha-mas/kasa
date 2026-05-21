34c2d62ef1dfc13adb51896dbd621bea4b771551
cha-mas <charles.masclef@outlook.com>
2026-05-21 14:27:33 +0200

    feat: added home cards

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router";
 export function PropertyCard({ data }) {
     return (
         <Link to="/">
-            <article className={styles.propertyCard}>{data.id}</article>
+            <article className={styles.propertyCard}>
+                <img src={data.cover} alt={data.title} />
+                <div className={styles.backdrop}></div>
+                <h3>{data.title}</h3>
+            </article>
         </Link>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import { Banner } from "../components/shared/Banner";
 import BGImage from "../assets/img/banners/1.jpg";
 import { useProperties } from '../hooks/useProperties'
+import { PropertyCard } from '../components/PropertyCard'
 
 export function Home() {
 
-    const { data, error } = useProperties()
+    const { data } = useProperties()
 
     return (
         <>
             <Banner image={BGImage}>Chez vous, partout et ailleurs</Banner>
-
+            <section className="properties-grid">
+                {data.map(property => (
+                    <PropertyCard data={property} />
+                ))}
+            </section>
         </>
     )
 }
