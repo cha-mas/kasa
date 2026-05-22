6d8bc622689206f2f78083050c1ebd99c4215baf
cha-mas <charles.masclef@outlook.com>
2026-05-22 08:52:30 +0200

    feat: added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,12 @@
+import styles from './Error.module.scss';
+import { Link } from "react-router";
+
 export function Error() {
-    return <>404</>
+    return (
+        <div className={styles.errorContainer}>
+            <b>404</b>
+            <h1>Oups! La page que vous demandez n'existe pas.</h1>
+            <Link to='/'>Retourner sur la page d'accueil</Link>
+        </div>
+    )
 }
