dc1c3c88e1160714589cef7c9085ddf881cf9560
cha-mas <charles.masclef@outlook.com>
2026-05-25 20:03:26 +0200

    fix: hooked details page with param id

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,14 @@
 import { useParams } from "react-router";
+import { useProperty } from '../hooks/properties'
 
 export function Details() {
     const params = useParams();
 
-    return <>{params.id}</>
+    const { data } = useProperty(params.id)
+
+    return (
+        <>
+            <pre>{JSON.stringify(data, undefined, 4)}</pre>
+        </>
+    )
 }
